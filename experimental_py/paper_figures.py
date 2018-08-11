b9aa0521d8a6b295ffdbdec0c6d5478b01b24597
loneharoon <haroonr@iiitd.ac.in>
2018-08-11 22:44:17 +0530

    plots for paper

---
+++ experimental_py/paper_figures.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+Created on Wed Aug  1 16:21:04 2018
+
+@author: haroonr
+"""
+%matplotlib inline
+import pandas as pd
+import matplotlib.pyplot as plt
